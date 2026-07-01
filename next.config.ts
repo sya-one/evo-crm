f7e5cada705faad8cbcf817e6b34b7e569bbb050
sya-one <choncosp@gmail.com>
2026-07-01 14:58:26 +0200

    updated nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone", // 🔥 REQUIRED for Docker production build
+
   images: {
     domains: ["localhost"],
   },
+
   experimental: {
     serverActions: {
       bodySizeLimit: "10mb",
